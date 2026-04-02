c508ecf9712ac34b2a657e892f76e1cd13d7e61a
professornaite <professornaite@gmail.com>
2026-04-02 12:28:33 -0400

    reviz_plot update

--- R/reviz_plot.R
+++ R/reviz_plot.R
@@ -12,18 +12,18 @@
 reviz_plot <- function(data, x, y, z = NULL, model = c("lm", "loess"), use_color = TRUE) {
   model <- match.arg(model)
 
-  p <- ggplot(data, aes_string(x = x, y = y)) +
-    geom_point(alpha = 0.7, size = 2) +
-    geom_smooth(method = model, se = TRUE, size = 1.2) +
-    theme_minimal()
+  p <- ggplot2::ggplot(data, ggplot2::aes(x = .data[[x]], y = .data[[y]])) +
+    ggplot2::geom_point(alpha = 0.7, size = 2) +
+    ggplot2::geom_smooth(method = model, se = TRUE, linewidth = 1.2) +
+    ggplot2::theme_minimal()
 
-  if (!is.null(z) && z != "") {
+  if (!is.null(z) && nzchar(z)) {
     if (use_color) {
-      p <- p + aes_string(color = z)
+      p <- p + ggplot2::aes(color = .data[[z]])
     } else {
-      p <- p + facet_wrap(as.formula(paste("~", z)))
+      p <- p + ggplot2::facet_wrap(stats::as.formula(paste("~", z)))
     }
   }
 
-  p + labs(title = paste(y, "~", x))
+  p + ggplot2::labs(title = paste(y, "~", x))
 }

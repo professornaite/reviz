af9e9ef898053db65381a79f8825c984ec70ce7c
professornaite <professornaite@gmail.com>
2026-04-02 14:48:55 -0400

    .

--- reviz-deploy/app.R
+++ reviz-deploy/app.R
@@ -1,17 +1,19 @@
 Sys.setenv(RENV_CONFIG_ACTIVE = "FALSE")
 
+if (!dir.exists("R-lib")) dir.create("R-lib")
+.libPaths(c(normalizePath("R-lib"), .libPaths()))
+
 if (!requireNamespace("remotes", quietly = TRUE)) {
-  install.packages("remotes")
+  install.packages("remotes", repos = "https://cloud.r-project.org")
 }
 
 if (!requireNamespace("critstats", quietly = TRUE)) {
-  remotes::install_github("professornaite/critstats", force = TRUE)
+  remotes::install_github("professornaite/critstats", force = TRUE, upgrade = "never")
 }
 
 if (!requireNamespace("reviz", quietly = TRUE)) {
-  remotes::install_local("../reviz_0.1.0.tar.gz", force = TRUE)
+  install.packages("reviz_0.1.0.tar.gz", repos = NULL, type = "source")
 }
 
 library(reviz)
-
 reviz::launch_reviz()

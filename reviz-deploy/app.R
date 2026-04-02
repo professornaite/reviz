5a25911c3152bdbadc265c94a9902a8c58f3ef55
professornaite <professornaite@gmail.com>
2026-04-02 13:17:17 -0400

    .

--- reviz-deploy/app.R
+++ reviz-deploy/app.R
@@ -1,7 +1,10 @@
+if (!requireNamespace("remotes", quietly = TRUE)) {
+  install.packages("remotes")
+}
+
+if (!requireNamespace("critstats", quietly = TRUE)) {
+  remotes::install_github("professornaite/critstats", force = TRUE)
+}
+
 library(reviz)
-library(ggplot2)
-library(remotes)
-remotes::install_github("professornaite/critstats", force=TRUE)
-library(critstats)
-library(MASS)
 launch_reviz()
